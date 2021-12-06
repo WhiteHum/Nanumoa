7d629ab88a6cbbed4b29105677690a9262460401
김현서 <zaaduu44@gmail.com>
2021-12-06 17:38:25 +0900

    수정3

--- src/test/java/com/example/nanumoa/NanumoaApplicationTests.java
+++ src/test/java/com/example/nanumoa/NanumoaApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.nanumoa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootTest
+@PropertySource("classpath:config.properties")
 class NanumoaApplicationTests {
 
     @Test
