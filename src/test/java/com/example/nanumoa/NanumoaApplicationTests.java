212cc5759a246deca96c0934ad7c455f216bff09
김현서 <zaaduu44@gmail.com>
2021-12-06 17:40:54 +0900

    테스트 없앤것

--- src/test/java/com/example/nanumoa/NanumoaApplicationTests.java
+++ src/test/java/com/example/nanumoa/NanumoaApplicationTests.java
@@ -1,15 +1,14 @@
-package com.example.nanumoa;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
-
-@SpringBootTest
-@PropertySource("classpath:config.properties")
-class NanumoaApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.nanumoa;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//
+//@SpringBootTest
+//class NanumoaApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
